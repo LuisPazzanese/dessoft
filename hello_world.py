b890e0d9f3ce8433539001034a451b1d646d01ae
LuisPazzanese <luisfpp1@al.insper.edu.br>
2023-10-31 23:13:38 -0300

    worlds_data

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,13 @@
-print('hello world')
+from words_data import sanitized_words
+from random import choice
+
+def inicializa(listaPal):
+    return {'n' : len(listaPal[0]),
+    'sorteada' : choice(listaPal),
+    'especuladas' : [],
+    'tentativas' : len(listaPal[0]) +1
+    }
+
+
+palavra = inicializa(sanitized_words)
+print(palavra)
