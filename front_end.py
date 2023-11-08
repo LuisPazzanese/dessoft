be973f97145bcf47060f9798004f1e7d6a1ff059
LuisPazzanese <luisfpp1@al.insper.edu.br>
2023-11-07 23:11:24 -0300

    add: funcao para aparecer os inputs

--- front_end.py
+++ front_end.py
@@ -2,6 +2,7 @@ from random import choice
 from colorama import init, Fore, Back, Style
 import words_data
 import time
+from art import *
 init(autoreset=True)
 
 
@@ -17,7 +18,6 @@ print('=' * 26 + '\n')
 
 while True:
     try:
-        global valor_numerico
         quant_letras = input('Quantas letras voce deseja que a palavra tenha? ')
         valor_numerico = int(quant_letras)
         break
@@ -39,8 +39,52 @@ print('    ♦ ' + Fore.BLACK+'Preto' + Fore.RESET + '  : a palavra não tem a l
 print('  - Os acentos são ignorados.')
 print('  - As palavras podem possuir letras repetidas.' + '\n')
 print('Sorteando uma palavra...\n')
-time.sleep(1)
+time.sleep(2)
 sorteada = info_dic['sorteada']
 print('Já tenho uma palavra! Tente adivinhá-la!')
 
+print(sorteada)
+
+    
+def jogo(palpite):
+    while True:
+        
+        retorno = ''
+
+        cores = words_data.inidica_posicao(sorteada,palpite)
+        for i in range(len(palpite)):
+            if cores[i] == 0:
+                retorno += Fore.BLUE + text2art(palpite[i]) + Fore.RESET
+            elif cores[i] == 1:
+                retorno += Fore.YELLOW +  text2art(palpite[i]) + Fore.RESET
+            else:
+                retorno += Fore.BLACK + text2art(palpite[i]) + Fore.RESET
+        print(retorno)
+        
+        return retorno
+        
+        
+while True:
+    if tentativas == 0:
+        print('Voce perdeu! ')
+        break
+    print('Voce tem {0} tentativas!'.format(tentativas))
+    palpite = input('Qual o seu palpite? ')
+    if len(palpite) != int(quant_letras):
+        print('Somente palavras de {0} letras'.format(quant_letras))
+    else:
+        retorno = jogo(palpite)
+        if palpite == sorteada:
+            print('Voce ganhou!')
+            break
+        tentativas -= 1
+        
+        
+
+rejogar = input('deseja jogar novamente? (s/n): ')
+
+if rejogar.lower() in ['s','sim','claro']:
+    jogo(input(palpite))
+elif rejogar.lower() in ['n','nao','não','nunca']:
+    print('Ate mais!')
 
